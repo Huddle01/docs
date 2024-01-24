de87b135cb9186957485582d72ecb1e42f1e6207
harry-sketch <harshbisht90@gmail.com>
2024-01-24 23:47:02 +0530

    add: examples

---
+++ components/Migration/data.tsx
@@ -0,0 +1,115 @@
+export const HooksData = [
+  {
+    old: "useRoom",
+    new: {
+      one: "useRoom",
+      urlOne: "../React/hooks/useRoom",
+    },
+  },
+  {
+    old: "useLobby",
+    new: {
+      one: "useLobby",
+      urlOne: "../React/hooks/useLobby",
+    },
+  },
+  {
+    old: "usePeers",
+    new: {
+      one: "usePeersIds",
+      urlOne: "../React/hooks/usePeerIds",
+    },
+  },
+  {
+    old: "useAudio",
+    new: {
+      one: "useLocalAudio",
+      two: "useRemoteAudio",
+      urlOne: "../React/hooks/useLocalAudio",
+      UrlTwo: "../React/hooks/useRemoteAudio",
+    },
+  },
+  {
+    old: "useVideo",
+    new: {
+      one: "useLocalVideo",
+      two: "useRemoteVideo",
+      urlOne: "../React/hooks/useLocalVideo",
+      UrlTwo: "../React/hooks/useRemoteVideo",
+    },
+  },
+  {
+    old: "useScreenShare",
+    new: {
+      one: "useLocalScreenShare",
+      two: "useRemoteScreenShare",
+      urlOne: "../React/hooks/useLocalScreenShare",
+      UrlTwo: "../React/hooks/useRemoteScreenShare",
+    },
+  },
+  {
+    old: "---",
+    new: {
+      one: "useRoomControls",
+      urlOne: "../React/hooks/useRoomControls",
+    },
+  },
+  {
+    old: "---",
+    new: {
+      one: "useDataMessage",
+      urlOne: "../React/hooks/useDataMessage",
+    },
+  },
+  {
+    old: "---",
+    new: {
+      one: "useDevices",
+      urlOne: "../React/hooks/useDevices",
+    },
+  },
+  {
+    old: "---",
+    new: {
+      one: "useActivePeers",
+      urlOne: "../React/hooks/useActivePeers",
+    },
+  },
+  {
+    old: "---",
+    new: {
+      one: "useRoomMetadata",
+      urlOne: "../React/hooks/useRoomMetadata",
+    },
+  },
+  {
+    old: "useHuddle01",
+    new: {
+      one: "---",
+    },
+  },
+  {
+    old: "useAppUtils",
+    new: {
+      one: "---",
+    },
+  },
+  {
+    old: "useAcl",
+    new: {
+      one: "---",
+    },
+  },
+  {
+    old: "useLivestream",
+    new: {
+      one: "---",
+    },
+  },
+  {
+    old: "useRecording",
+    new: {
+      one: "---",
+    },
+  },
+];
