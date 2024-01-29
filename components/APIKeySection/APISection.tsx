f4a9b266a8fcfb0aae002e2991a71abd455b0d96
vrajdesai78 <vrajdesai78@gmail.com>
2024-01-30 00:15:03 +0530

    vraj/dev-1734-make-project-name-a-mandatory-requirement-in-sdk-docs

--- components/APIKeySection/APISection.tsx
+++ components/APIKeySection/APISection.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { toast } from 'react-hot-toast';
 
 import { ConnectButton } from '@rainbow-me/rainbowkit';
@@ -6,14 +6,14 @@ import { useMutation } from '@tanstack/react-query';
 import axios from 'axios';
 import { useAccount, useDisconnect } from 'wagmi';
 
-import { cn } from '../../helpers/utils';
+import { cn, extractProjectName } from '../../helpers/utils';
 import DomainInputStrip from './DomainInputStrip';
 import KeyStrip from './KeyStrip';
 
 const APISection = () => {
   const [apiKey, setApiKey] = useState('');
   const [projectId, setProjectId] = useState('');
-  const [exisitingDomain, setExisitingDomain] = useState<string | null>(null);
+  const [projectName, setProjectName] = useState<string | null>(null);
   const { disconnect } = useDisconnect();
 
   const { mutateAsync: fetchApiKey } = useMutation({
@@ -42,46 +42,58 @@ const APISection = () => {
       });
       return data;
     },
-    onSuccess: ({ apiKey, message, projectId, domain }) => {
+    onSuccess: ({ apiKey, projectId, domain }) => {
       setApiKey(apiKey);
       setProjectId(projectId);
-      toast.success(message);
       if (!domain || !domain.length) return;
-      setExisitingDomain(domain);
+      const name = extractProjectName(domain);
+      setProjectName(name);
     },
-    onError: err => {
+    onError: (err) => {
       disconnect();
       console.error(err);
       toast.error('Error getting API Key');
     },
   });
 
-  const { isConnected } = useAccount({
+  const { isConnected, status } = useAccount({
     onConnect({ address }) {
       if (address) {
         fetchApiKey({
           address,
           chain: 'ETHEREUM',
         })
-          .then(res => {
+          .then((res) => {
             console.log(res);
           })
-          .catch(err => {
+          .catch((err) => {
             console.log(err);
           });
       }
     },
+    onDisconnect() {
+      setProjectName(null);
+    },
   });
 
-  return (
-    <div className="flex flex-col gap-6 mt-10 justify-center items-center w-full">
-      <ConnectButton />
+  useEffect(() => {
+    console.log('status', status);
+  }, [status]);
 
+  return (
+    <div className='flex flex-col gap-6 mt-10 justify-center items-center w-full'>
+      <ConnectButton />
+      <span className='font-bold text-lg'>{projectName}</span>
       {isConnected && (
         <div className={cn('flex flex-col items-center gap-5 w-full')}>
-          <KeyStrip label="API Key" text={apiKey} />
-          <KeyStrip label="Project ID" text={projectId} />
-          <DomainInputStrip exisitingDomain={exisitingDomain} apiKey={apiKey} />
+          {projectName?.length > 0 ? (
+            <>
+              <KeyStrip label='API Key' text={apiKey} />
+              <KeyStrip label='Project ID' text={projectId} />
+            </>
+          ) : (
+            <DomainInputStrip apiKey={apiKey} setProjectName={setProjectName} />
+          )}
         </div>
       )}
     </div>
