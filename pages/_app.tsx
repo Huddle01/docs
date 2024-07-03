5178e0aeb6128fb86601ef55c6c4ca8a3f9d7d11
Mizan Ali <mizanalip786@gmail.com>
2024-07-03 08:02:11 +0530

    remove unused dependencies

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,7 @@ import "../styles/global.css";
 
 import { Toaster } from "react-hot-toast";
 
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { NextLayoutComponentType } from "next";
-
-const client = new QueryClient();
 
 export default function Nextra({
   Component,
@@ -17,10 +14,8 @@ export default function Nextra({
   const getLayout = Component.getLayout || ((page: React.ReactElement) => page);
   return (
     <>
-      <QueryClientProvider client={client}>
-        {getLayout(<Component {...pageProps} />)}
-        <Toaster position="bottom-right" />
-      </QueryClientProvider>
+      {getLayout(<Component {...pageProps} />)}
+      <Toaster position="bottom-right" />
     </>
   );
 }
