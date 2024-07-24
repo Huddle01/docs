7a27500ae5214399000af32f5409ac14fb643941
Mizan Ali <mizanalip786@gmail.com>
2024-07-25 02:53:49 +0530

    update react native docs

--- components/common/Footer/Description.tsx
+++ components/common/Footer/Description.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import React from "react";
 
 const Description = () => (
   <div className="text-slate-400 md:text-sm font-medium md:max-w-[16.875rem] my-5">
-    Audio/Video Infrastructure designed for the developers to empower them ship
+    Audio/Video Infrastructure designed for developers to empower them to ship
     simple yet powerful Audio/Video Apps.
   </div>
 );
