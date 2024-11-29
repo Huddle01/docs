64de263940fc0d7dfb31340cd1211a01a8a585c1
mizanxali <mizanalip786@gmail.com>
2024-11-29 11:20:15 +0530

    update font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
 import '../styles/global.css';
+import { Chakra_Petch } from 'next/font/google';
 
 import { Toaster } from 'react-hot-toast';
 
 import type { NextLayoutComponentType } from 'next';
+
+const chakra = Chakra_Petch({
+  weight: ['400', '500', '600', '700'],
+  subsets: ['latin'],
+});
 
 export default function Nextra({
   Component,
@@ -13,9 +19,9 @@ export default function Nextra({
 }) {
   const getLayout = Component.getLayout || ((page: React.ReactElement) => page);
   return (
-    <>
+    <main className={chakra.className}>
       {getLayout(<Component {...pageProps} />)}
       <Toaster position="bottom-right" />
-    </>
+    </main>
   );
 }
